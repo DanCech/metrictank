c37642396f5364935bc5051001d516155cf2f80e
Mauro Stettler <mauro.stettler@gmail.com>
2017-12-08 18:47:58 +0900

    add comments and fix synchronization issue

--- idx/memory/memory_find_test.go
+++ idx/memory/memory_find_test.go
@@ -959,7 +959,7 @@ func BenchmarkTagQueryKeysByPrefixExpressions(b *testing.B) {
 		tagPrefix: "di",
 		expr:      []string{"metric=~.*_time$", "direction!=~re", "host=~host9[0-9]0"},
 		from:      12345,
-		expRes:    []string{"disk", "direction"},
+		expRes:    []string{"direction", "disk"},
 	}
 
 	b.ReportAllocs()
